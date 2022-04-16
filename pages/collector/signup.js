c544c6be30900b50561cd3edabb2a1376ca4abaa
MEGA-BLASTER <devmegablaster@gmail.com>
2022-04-16 17:41:50 +0530

    Minor Fixes

--- pages/collector/signup.js
+++ pages/collector/signup.js
@@ -3,6 +3,7 @@ import SignUp from '../../components/Collector/SignUp/SignUp'
 import Router from 'next/router'
 import db from '../../firebase'
 import firebase from 'firebase'
+import { Loader } from '@mantine/core'
 
 function signup() {
   const [loading, setLoading] = useState(true)
