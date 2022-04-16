43c8ee7d721b2e3c6ce1b7ab9229a3b0791905c5
MEGA-BLASTER <devmegablaster@gmail.com>
2022-04-16 17:18:49 +0530

    Basic Bug Fixes

--- pages/collector/signin.js
+++ pages/collector/signin.js
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SignIn from '../../components/Collector/SignIn/SignIn'
+import { Loader } from '@mantine/core'
+import Router from 'next/router'
+import db from '../../firebase'
+import firebase from 'firebase'
 
 function signin() {
-  return (
-    <div className="h-screen w-screen">
-      <SignIn />
-    </div>
-  )
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        db.collection('Collectors')
+          .doc(user.uid)
+          .get()
+          .then((data) => {
+            if (data.exists) {
+              Router.push('/collector/dashboard')
+            } else {
+              setLoading(false)
+              firebase.auth().signOut()
+            }
+          })
+      } else {
+        setLoading(false)
+      }
+    })
+  })
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader color="green" size="xl" />
+      </div>
+    )
+  } else {
+    return (
+      <div className="h-screen w-screen">
+        <SignIn />
+      </div>
+    )
+  }
 }
 
 export default signin
--- pages/collector/signup.js
+++ pages/collector/signup.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SignUp from '../../components/Collector/SignUp/SignUp'
+import Router from 'next/router'
+import db from '../../firebase'
+import firebase from 'firebase'
 
 function signup() {
-  return (
-    <div className="h-screen w-screen">
-      <SignUp />
-    </div>
-  )
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        db.collection('Collectors')
+          .doc(user.uid)
+          .get()
+          .then((data) => {
+            if (data.exists) {
+              Router.push('/collector/dashboard')
+            } else {
+              setLoading(false)
+              firebase.auth().signOut()
+            }
+          })
+      } else {
+        setLoading(false)
+      }
+    })
+  })
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Loader color="green" size="xl" />
+      </div>
+    )
+  } else {
+    return (
+      <div className="h-screen w-screen">
+        <SignUp />
+      </div>
+    )
+  }
 }
 
 export default signup
