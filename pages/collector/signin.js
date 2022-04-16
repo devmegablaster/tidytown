c7de287a6d7b00a0070537fbe32125ab4d51f4a8
MEGA-BLASTER <devmegablaster@gmail.com>
2022-04-16 18:56:05 +0530

    Hopefully the last commit

--- pages/collector/signin.js
+++ pages/collector/signin.js
@@ -7,38 +7,12 @@ import firebase from 'firebase'
 
 function signin() {
   const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        db.collection('Collectors')
-          .doc(user.uid)
-          .get()
-          .then((data) => {
-            if (data.exists) {
-              Router.push('/collector/dashboard')
-            } else {
-              setLoading(false)
-              firebase.auth().signOut()
-            }
-          })
-      } else {
-        setLoading(false)
-      }
-    })
-  })
-  if (loading) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <Loader color="green" size="xl" />
-      </div>
-    )
-  } else {
-    return (
-      <div className="h-screen w-screen">
-        <SignIn />
-      </div>
-    )
-  }
+
+  return (
+    <div className="h-screen w-screen">
+      <SignIn />
+    </div>
+  )
 }
 
 export default signin
--- pages/collector/signup.js
+++ pages/collector/signup.js
@@ -6,39 +6,11 @@ import firebase from 'firebase'
 import { Loader } from '@mantine/core'
 
 function signup() {
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        db.collection('Collectors')
-          .doc(user.uid)
-          .get()
-          .then((data) => {
-            if (data.exists) {
-              Router.push('/collector/dashboard')
-            } else {
-              setLoading(false)
-              firebase.auth().signOut()
-            }
-          })
-      } else {
-        setLoading(false)
-      }
-    })
-  })
-  if (loading) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <Loader color="green" size="xl" />
-      </div>
-    )
-  } else {
-    return (
-      <div className="h-screen w-screen">
-        <SignUp />
-      </div>
-    )
-  }
+  return (
+    <div className="h-screen w-screen">
+      <SignUp />
+    </div>
+  )
 }
 
 export default signup
